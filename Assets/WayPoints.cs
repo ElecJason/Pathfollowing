c5d273d050ffc6f5a7c75a63a3e29f6d16ac99b5
Jason Dikken <electronicjason.dikken@gmail.com>
2020-09-07 15:24:30 +0200

    Initial commit

---
+++ Assets/WayPoints.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private int waypointIndex;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            gameObject.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+            Vector3 start = new Vector3(player.transform.position.x, 1, player.transform.position.z);
+            player.transform.position = Vector3.MoveTowards(start, waypoints[waypointIndex].gameObject.transform.position, Time.deltaTime * speed);
+
+            if (player.transform.position == waypoints[waypointIndex].gameObject.transform.position)
+            {
+                Debug.Log("Waypoint" +waypointIndex);
+                waypointIndex++;
+            }
+        }
+    }
+
